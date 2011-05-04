3b65678073dd766bf42777757a0bc9777383327e
marius a. eriksen <marius@twitter.com>
2011-05-03 23:17:00 -0700

    fully cohttp http_client

--- src/twitterstream_stream.ml
+++ src/twitterstream_stream.ml
@@ -1,70 +1,80 @@
-(*pp camlp4o -I `ocamlfind query lwt.syntax` pa_lwt.cmo *)
-
 open Lwt
 open Printf
 open Json_type
+open Cohttp
 
-(* TODO: take care of delete messages *)
+exception Stream_http_error of int
+exception Stream_tcp_error
 
-let min_reconnect_interval = 5.  (* seconds *)
+type reconnect_policy = {
+  max_tries         : int;
+  max_tries_per_sec : int;
+}
+
+module Throttler = Lwt_throttle.Make(
+  (* a singleton throttler. *)
+  struct
+    type t = unit
+    let equal () () = true
+    let hash () = 0
+  end)
 
 let url_of_type = function
   | `Firehose -> "http://stream.twitter.com/1/statuses/firehose.json"
   | `Sample   -> "http://stream.twitter.com/1/statuses/sample.json"
   | `Custom url -> url
 
-let do_http_fetch (username, password) url fd =
-  let channel_writer data = Unix.write fd data 0 (String.length data)
-  and error_buffer = ref "" in
-  Curl.global_init Curl.CURLINIT_GLOBALALL;
-  try
-    let connection = Curl.init () in
-    Curl.set_errorbuffer connection error_buffer;
-    Curl.set_writefunction connection channel_writer;
-    Curl.set_url connection (url_of_type url);
-    Curl.setopt connection (Curl.CURLOPT_HTTPAUTH [Curl.CURLAUTH_ANY]);
-    Curl.set_userpwd connection (sprintf "%s:%s" username password);
-    Curl.perform connection;
-    Curl.cleanup connection
-  with
-    | Curl.CurlException (reason, code, str) ->
-        fprintf stderr "Error: %s\n" !error_buffer
-    | Failure s ->
-        fprintf stderr "Caught exception: %s\n" s;
-        Curl.global_cleanup ()
+let stream_of_channel chan =
+  let lines = Lwt_io.read_lines chan in
+  let map_line line =
+    return (Twitterstream_message.of_json_string line) in
+  Lwt_stream.map_s map_line lines
 
-let rec fork_http_fetcher' ?(connect_time = 0.) auth url out_fd =
-  match Unix.fork () with
-    | 0 -> do_http_fetch auth url out_fd; return ()
-    | child_pid ->
-        Lwt_unix.waitpid [] child_pid >> 
-        let time_left =
-          min_reconnect_interval -. (Unix.time () -. connect_time) in
-        begin  (* ensure that we aren't too aggressive with reconnects. *)
-          if time_left > 0.
-          then Lwt_unix.sleep time_left
-          else return ()
-        end >>
-        fork_http_fetcher' ~connect_time:(Unix.time ()) auth url out_fd
+let connect (user, pass) url_type =
+  let (input, output) = Lwt_io.pipe () in
+  let stream = stream_of_channel input in
 
-let fork_http_fetcher auth url =
-  let (in_fd, out_fd) = Unix.pipe () in
-  let monitor_t = fork_http_fetcher' auth url out_fd in
-  monitor_t, Lwt_io.of_unix_fd Lwt_io.input in_fd
+  let t = begin
+    let userpass_str = Base64.str_encode (user ^ ":" ^ pass) in
+    let headers = [
+      "Host", "stream.twitter.com";
+      "Authorization", "Basic " ^ userpass_str
+    ] in
 
-let rec push_to_stream push_stream chan =
-  try_bind (fun () -> Lwt_io.read_line chan) 
-    begin fun line ->
-      push_stream (Some (Twitterstream_message.of_json_string line));
-      push_to_stream push_stream chan
-    end 
-    (fun ex (* only on EOF? *) -> push_stream None; return ())
+    try_lwt
+      Http_client.get_to_chan (url_of_type url_type) ~headers output >>
+      return ()
+    with
+      | Http_client.Tcp_error (source, _) ->
+          fail Stream_tcp_error
+      | Http_client.Http_error (code, _, _) ->
+          fail (Stream_http_error code)
+    finally
+      Lwt_io.close output
+  end in
 
-let of_channel channel =
-  let (stream, push_stream) = Lwt_stream.create () in
-  push_to_stream push_stream channel, stream
+  t, stream
 
-let of_http_stream userpass url_type =
-  let monitor_t, channel = fork_http_fetcher userpass url_type in
-  let t, stream = of_channel channel in
-  join [monitor_t; t], stream
+let reconnect policy auth url_type =
+  let stream, push = Lwt_stream.create () in
+  let throttler = Throttler.create
+    ~rate:policy.max_tries_per_sec
+    ~max:(policy.max_tries_per_sec * policy.max_tries)
+    ~n:1 in
+  let rec go state =
+    try_lwt
+      (* TODO: success resets the tries? *)
+      let (t, stream) = connect auth url_type in
+      join [Lwt_stream.iter (fun item -> push (Some item)) stream; t]
+    with
+      | exc ->
+          if state.max_tries > 1 then
+            Throttler.wait throttler () >>
+            go { state with max_tries = state.max_tries - 1 }
+          else
+            (push None; fail exc)
+  in go policy, stream
+
+let open_stream
+  ?(reconnect_policy = {max_tries = 1; max_tries_per_sec = 1})
+  auth url_type = reconnect reconnect_policy auth url_type
--- src/twitterstream_stream.mli
+++ src/twitterstream_stream.mli
@@ -1,9 +1,18 @@
 
-val of_channel : 
-  Lwt_io.input Lwt_io.channel -> 
-  unit Lwt.t * Twitterstream_message.t Lwt_stream.t
+exception Stream_http_error of int
+exception Stream_tcp_error
 
-val of_http_stream : 
+type reconnect_policy = {
+  max_tries         : int;
+  max_tries_per_sec : int;
+}
+
+val stream_of_channel : 
+  Lwt_io.input Lwt_io.channel -> 
+  Twitterstream_message.t Lwt_stream.t
+
+val open_stream :
+  ?reconnect_policy:reconnect_policy ->
   string * string -> 
-  [< `Custom of string | `Firehose | `Sample ] -> 
+  [ `Custom of string | `Firehose | `Sample ] -> 
   unit Lwt.t * Twitterstream_message.t Lwt_stream.t
